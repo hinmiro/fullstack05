f6b4c5e94dacc44864487a52fbd5b0c1ce779f95
mirohi <m_hintikka@hotmail.com>
2024-08-04 22:33:10 +0300

    Add assignment 5.14

--- bloglist-frontend/tests/Blog.test.jsx
+++ bloglist-frontend/tests/Blog.test.jsx
@@ -1,15 +1,43 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '/src/components/Blog.jsx'
 
-test('blog to render title and author', () => {
-  const blog = {
-    title: 'Test blog',
-    author: 'tester',
-    likes: 0,
-    url: 'asddsad',
-    user: { id: '123' },
-  }
+const blog = {
+  id: '123asd',
+  title: 'Test blog',
+  author: 'tester',
+  likes: 1,
+  url: 'asddsad',
+  user: { id: '123' },
+}
 
+beforeEach(() => {
+  const localStorageMock = (function () {
+    let store = {}
+    return {
+      getItem(key) {
+        return store[key] || null
+      },
+      setItem(key, value) {
+        store[key] = value.toString()
+      },
+      clear() {
+        store = {}
+      },
+      removeItem(key) {
+        delete store[key]
+      },
+    }
+  })()
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+
+  window.localStorage.setItem(
+    'appUser',
+    JSON.stringify({ id: '123', username: 'tester', blogs: ['123asd'] })
+  )
+})
+
+test('blog to render title and author', () => {
   render(
     <Blog
       blog={blog}
@@ -22,4 +50,29 @@ test('blog to render title and author', () => {
   expect(
     screen.getByText((content) => content.startsWith('tester'))
   ).toBeInTheDocument()
+})
+
+test('blog to render url, likes, etc. after clicking more', async () => {
+  render(
+    <Blog
+      blog={blog}
+      updateBlog={() => {}}
+      removeBlog={() => {}}
+      user={{ id: '123' }}
+    />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('more')
+  await user.click(button)
+
+  screen.debug()
+
+  expect(screen.getByText('Title: Test blog')).toBeInTheDocument()
+  expect(screen.getByText('Author: tester')).toBeInTheDocument()
+
+  expect(
+    screen.getByText((content) => content.includes('Likes: 1'))
+  ).toBeInTheDocument()
+  expect(screen.getByText('URl: asddsad')).toBeInTheDocument()
 })
